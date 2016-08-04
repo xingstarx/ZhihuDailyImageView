3b94b49ec10835bb7f01675b9c956a872c0d4e29
xingxing <xxx823952375@gmail.com>
2016-08-05 00:05:52 +0800

    add gestureDetector , scaleGestureDetector

--- app/src/main/java/com/github/star/imageview/MainActivity.java
+++ app/src/main/java/com/github/star/imageview/MainActivity.java
@@ -1,21 +1,45 @@
 package com.github.star.imageview;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private ImageView mImageView;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private GestureDetector mGestureDetector;
+    @NonNull
+    private GestureDetector.SimpleOnGestureListener mSimpleOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return true;
+        }
+    };
+    @NonNull
+    private ScaleGestureDetector.SimpleOnScaleGestureListener mSimpleOnScaleGestureListener = new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mImageView = (ImageView) findViewById(R.id.image_view);
+        mScaleGestureDetector = new ScaleGestureDetector(this, mSimpleOnScaleGestureListener);
+        mGestureDetector = new GestureDetector(this, mSimpleOnGestureListener);
+        mImageView.setOnTouchListener(this);
         Picasso.with(this)
                 .load("http://pic4.zhimg.com/70/f3bfac899b2e83f2f7c5aaa9814814f3_b.jpg")
                 .fit().centerInside()
@@ -28,5 +52,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onError() {
                     }
                 });
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        boolean flag = mGestureDetector.onTouchEvent(event);
+        flag |= mScaleGestureDetector.onTouchEvent(event);
+        return flag;
     }
 }

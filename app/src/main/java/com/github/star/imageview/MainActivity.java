c0a452050bd55a6569646a880fe10b4588b270d9
xingxing <xxx823952375@gmail.com>
2016-08-06 16:25:56 +0800

    set GestureDetector in MatrixImageView

--- app/src/main/java/com/github/star/imageview/MainActivity.java
+++ app/src/main/java/com/github/star/imageview/MainActivity.java
@@ -1,47 +1,22 @@
 package com.github.star.imageview;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.view.View;
 
 import com.github.star.imageview.widget.MatrixImageView;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private MatrixImageView mImageView;
-    private ScaleGestureDetector mScaleGestureDetector;
-    private GestureDetector mGestureDetector;
-
-    @NonNull
-    private GestureDetector.SimpleOnGestureListener mSimpleOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
-        @Override
-        public boolean onDoubleTap(MotionEvent e) {
-            return true;
-        }
-    };
-    @NonNull
-    private ScaleGestureDetector.SimpleOnScaleGestureListener mSimpleOnScaleGestureListener = new ScaleGestureDetector.SimpleOnScaleGestureListener() {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            return true;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mImageView = (MatrixImageView) findViewById(R.id.image_view);
-        mScaleGestureDetector = new ScaleGestureDetector(this, mSimpleOnScaleGestureListener);
-        mGestureDetector = new GestureDetector(this, mSimpleOnGestureListener);
-        mImageView.setOnTouchListener(this);
         Picasso.with(this)
                 .load("http://pic4.zhimg.com/70/f3bfac899b2e83f2f7c5aaa9814814f3_b.jpg")
                 .fit().centerInside()
@@ -55,12 +30,5 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                     public void onError() {
                     }
                 });
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        boolean flag = mGestureDetector.onTouchEvent(event);
-        flag |= mScaleGestureDetector.onTouchEvent(event);
-        return flag;
     }
 }
--- app/src/main/java/com/github/star/imageview/widget/MatrixImageView.java
+++ app/src/main/java/com/github/star/imageview/widget/MatrixImageView.java
@@ -3,7 +3,12 @@ package com.github.star.imageview.widget;
 import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.ImageView;
 
 /**
@@ -11,11 +16,27 @@ import android.widget.ImageView;
  * @desc support scale imageView
  */
 
-public class MatrixImageView extends ImageView {
+public class MatrixImageView extends ImageView implements View.OnTouchListener{
     private Matrix mTempMatrix = new Matrix();
     private Matrix mCurrentMatrix = new Matrix();
     private float[] mTempValues = new float[9];
     private ImageView.ScaleType mScaleType = ScaleType.MATRIX;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private GestureDetector mGestureDetector;
+    @NonNull
+    private GestureDetector.SimpleOnGestureListener mSimpleOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return true;
+        }
+    };
+    @NonNull
+    private ScaleGestureDetector.SimpleOnScaleGestureListener mSimpleOnScaleGestureListener = new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            return true;
+        }
+    };
 
     public MatrixImageView(Context context) {
         this(context, null);
@@ -35,6 +56,9 @@ public class MatrixImageView extends ImageView {
         if (mScaleType != getScaleType()) {
             setScaleType(mScaleType);
         }
+        mScaleGestureDetector = new ScaleGestureDetector(context, mSimpleOnScaleGestureListener);
+        mGestureDetector = new GestureDetector(context, mSimpleOnGestureListener);
+        setOnTouchListener(this);
     }
 
     public void update() {
@@ -69,4 +93,10 @@ public class MatrixImageView extends ImageView {
         return getDrawable().getIntrinsicHeight();
     }
 
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        boolean flag = mGestureDetector.onTouchEvent(event);
+        flag |= mScaleGestureDetector.onTouchEvent(event);
+        return flag;
+    }
 }
